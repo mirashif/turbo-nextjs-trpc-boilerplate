71b76e324544381e1e94b32689a347a38f0ffade
Mir Ashif <mirashif01@gmail.com>
2025-05-03 18:54:47 +0600

    rm schema types and todo

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,40 +1,34 @@
-import { relations } from "drizzle-orm";
-import { integer } from "drizzle-orm/pg-core";
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm"
+import { integer } from "drizzle-orm/pg-core"
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core"
 
 export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  createdAt: timestamp("created_at").defaultNow(),
-  email: text("email").notNull().unique(),
-  password: text("password").notNull(),
-});
+	id: serial("id").primaryKey(),
+	createdAt: timestamp("created_at").defaultNow(),
+	email: text("email").notNull().unique(),
+	password: text("password").notNull(),
+})
 
 export const usersRelations = relations(users, ({ many }) => ({
-  projects: many(projects),
-}));
+	projects: many(projects),
+}))
 
 export const projects = pgTable("projects", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at")
-    .notNull()
-    .defaultNow()
-    .$onUpdateFn(() => new Date()),
-  userId: integer("user_id")
-    .references(() => users.id, { onDelete: "cascade" })
-    .notNull(),
-});
+	id: serial("id").primaryKey(),
+	name: text("name").notNull(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	updatedAt: timestamp("updated_at")
+		.notNull()
+		.defaultNow()
+		.$onUpdateFn(() => new Date()),
+	userId: integer("user_id")
+		.references(() => users.id, { onDelete: "cascade" })
+		.notNull(),
+})
 
 export const projectsRelations = relations(projects, ({ one }) => ({
-  user: one(users, {
-    fields: [projects.userId],
-    references: [users.id],
-  }),
-}));
-
-/* TODO: need to check trpc setup. trpc inference isn't working.. :'( */
-export type Project = typeof projects.$inferSelect;
-export type NewProject = typeof projects.$inferInsert;
-export type User = typeof users.$inferSelect;
-export type NewUser = typeof users.$inferInsert;
+	user: one(users, {
+		fields: [projects.userId],
+		references: [users.id],
+	}),
+}))
